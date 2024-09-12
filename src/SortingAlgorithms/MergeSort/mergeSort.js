fbe853e494549b7d3b0709fd67a0771008cb6dc9
monajstha <manojstha700@gmail.com>
2024-09-12 23:37:48 +0545

    chore: proper comments on mergeSort

--- src/SortingAlgorithms/MergeSort/mergeSort.js
+++ src/SortingAlgorithms/MergeSort/mergeSort.js
@@ -1,13 +1,13 @@
 function merge(arr, start, mid, end) {
-  //   Length of temp arrays
-  let n1 = mid - start + 1;
-  let n2 = end - mid;
+  // Calculate the length for temp arrays
+  const n1 = mid - start + 1;
+  const n2 = end - mid;
 
-  //   Create temp arrays
+  // Create temp arrays
   const leftArr = new Array(n1);
   const rightArr = new Array(n2);
 
-  //   Copy data to temp arrays leftArr and rightArr
+  // Copy all the array elements to temp arrays
   for (let i = 0; i < n1; i++) {
     leftArr[i] = arr[start + i];
   }
@@ -16,11 +16,11 @@ function merge(arr, start, mid, end) {
     rightArr[j] = arr[mid + 1 + j];
   }
 
+  // Sort and merge the temp arrays back into the arr
   let i = 0,
     j = 0;
   let k = start;
 
-  //   Sort and merge the temp arrays back into the arr
   while (i < n1 && j < n2) {
     if (leftArr[i] < rightArr[j]) {
       arr[k++] = leftArr[i++];
@@ -29,7 +29,7 @@ function merge(arr, start, mid, end) {
     }
   }
 
-  //   Copy the remaining elements
+  // Copy the remianing elements
   for (; i < n1; i++) {
     arr[k++] = leftArr[i];
   }
@@ -38,19 +38,26 @@ function merge(arr, start, mid, end) {
     arr[k++] = rightArr[j];
   }
 
-  //   Return the sorted arr
+  //   Return the sorted array
   return arr;
 }
 
-function mergeSort(arr, start, end) {
+function mergeSort(arr, start = 0, end = arr.length - 1) {
+  // Return if there is only one element in the array
   if (start >= end) return;
 
+  //  Calculate the mid of the array
   const mid = Math.floor(start + (end - start) / 2);
 
+  //   Divide the left half
   mergeSort(arr, start, mid);
+
+  //   Divide the right half
   mergeSort(arr, mid + 1, end);
+
+  //   Merge the arrays
   return merge(arr, start, mid, end);
 }
 
-const arr = [12, 11, 13, 5, 6, 7];
-console.log(mergeSort(arr, 0, arr.length - 1));
+let arr = [3, 2, 1, 13, 8, 5, 0, 1];
+console.log(mergeSort(arr));
